b1f2db13ddc3bc3e092d36b424f0926b647eafe1
Eduardo Cunha <edurcunha@gmail.com>
2021-11-20 13:17:47 -0300

    Update

--- cScorePairWisePerGroup.R
+++ cScorePairWisePerGroup.R
@@ -11,8 +11,21 @@
 # site: 
 # spp.group: 
 # occurrence.tresh: 
+# standardize
 
-cScorePairWisePerGroup <- function(m, time, site, spp.group, occurrence.tresh) {
+cScorePairWisePerGroup <- function(m, time, site, spp.group, 
+                                   occurrence.tresh, ...) {
+  
+
+  extra.arg <- list(...)
+  if( "standardize" %in% names(extra.arg) ) {
+    standardize <- extra.arg[["standardize"]]
+  } else {
+    standardize <- FALSE 
+  }
+  
+  dataset <- matrix(0, 0, 5, dimnames = list( NULL, 
+    c('Site', 'Time', 'Spp.Groups', 'Spp.names', 'c-score')) )
   
   site.lab <- levels( as.factor(site) )
   
@@ -20,34 +33,17 @@ cScorePairWisePerGroup <- function(m, time, site, spp.group, occurrence.tresh) {
   
   spp.group.lab <- levels( as.factor(spp.group) )
   
-  
-  # spp.group.contrasts <- list()
-  # 
-  # for(i in seq_along(spp.group.lab)) {
-  #   
-  #   for(j in i:length(spp.group.lab)) {
-  #     
-  #     spp.group.contrasts <- c(spp.group.contrasts, 
-  #                              list( sort( c(spp.group.lab[i], 
-  #                                            spp.group.lab[j]) ) ) )
-  #     
-  #   } 
-  #   
-  # }
-  
   for(i in seq_along(site.lab) ) {
     
     for(j in seq_along(time.lab) ) {
       
-      m.tmp <- m[site == site.lab[i] && time == time.lab[j], ]
+      m.tmp <- m[site == site.lab[i] & time == time.lab[j], ]
       
       spp.include <- colSums(m.tmp) >= occurrence.tresh
       
-      samples.include <- rowSums(m.tmp) > 0
+      m.tmp <- m.tmp[ , spp.include, drop=F]      
       
-      m.tmp <- m.tmp[samples.include, spp.include, drop=F]      
-      
-      if( ncol(m.tmp) > 1 && nrow(m.tmp) > 1 ) {
+      if( ncol(m.tmp) > 1 & nrow(m.tmp) > 1 ) {
         
         n.spp <- ncol(m.tmp)
         
@@ -55,7 +51,8 @@ cScorePairWisePerGroup <- function(m, time, site, spp.group, occurrence.tresh) {
         
         spp.group.tmp <- spp.group[spp.include]  
         
-        c.score.pairwise.tmp <- c( cScorePairWise(m.tmp) )
+        c.score.pairwise.tmp <- c( cScorePairWise(m.tmp,
+                                                  standardize = standardize) )
         
         index.pos.tmp <- seq_along(c.score.pairwise.tmp)
         
@@ -69,23 +66,30 @@ cScorePairWisePerGroup <- function(m, time, site, spp.group, occurrence.tresh) {
         
         sp.pos <- cbind(sp.pos1, sp.pos2)
         
-        tags.tmp <- apply(sp.pos, MARGIN = 1, function(x) {
+        tags.tmp <- sapply(1:nrow(sp.pos), function(x) {
           
-          spp.order <- order(spp.group.tmp[x[1]], spp.group.tmp[x[2]])
+          spp.order <- order( c( spp.group.tmp[sp.pos[x,1]], 
+                             spp.group.tmp[sp.pos[x,2]] ) )
           
-          tags.tmp <- c( paste(spp.group.tmp[spp.order], collapse = ' vs '),
+          tags.tmp <- c( 
+            paste(spp.group.tmp[sp.pos[x,][spp.order]], 
+                               collapse = ' vs '),
+            paste(spp.names[sp.pos[x,][spp.order]], 
+                  collapse = ' vs ') )
           
-          paste(spp.names[spp.order], collapse = ' vs ') )
-          
-          return(tags.tmp)
+          return( tags.tmp )
 
-        } )
+        }, simplify = T )
         
-        ############## Continuar
-        tags.tmp
-        c.score.vals
-        site.lab[i]
-        time.lab[j]
+        tags.tmp <- t(tags.tmp)
+        
+        dataset.tmp <- cbind(
+              rep( site.lab[i], nrow(sp.pos) ), 
+              rep( time.lab[j], nrow(sp.pos) ),
+              tags.tmp,
+              c.score.vals)
+        
+        dataset <- rbind(dataset, dataset.tmp)
         
         }
       
