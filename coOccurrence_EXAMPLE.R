1c058b585cdf05090b2bdfbe9cbf01fef9a6083c
Eduardo Cunha <edurcunha@gmail.com>
2021-11-24 09:04:38 -0300

    Example Update

--- coOccurrence_EXAMPLE.R
+++ coOccurrence_EXAMPLE.R
@@ -13,29 +13,38 @@
 # ('https://github.com/edurcunha/coOccurrence') into your 
 # R project folder.
 
-# Loads the package
+# Load the package
 source('coOccurrence.R')
 
-# Creates a presence=absence matrix of species by samples
+# Create a presence=absence matrix of species by samples
 m <- matrix( rbinom(200, 1, 0.5), 20, 10)   
 
-# Assigns species names 
+# Assign species names 
 colnames(m) <- LETTERS[1:10]               
 
-# Creates a vector indicating the time period of the samples 
+# Create a vector indicating the time period of the samples 
 time <- rep( c("After", "Before"), each = 10)
 
-# Creates a vector indicating the site of the samples
+# Create a vector indicating the site of the samples
 site <- rep( rep( c("Parana", "Baia"), each = 5), 2 )
 
-# Creates a vector indicating the group the species belong 
+# Create a vector indicating the group the species belong 
 spp.group <- rep( c("native", "non-native"), each = 5)
 
-# Assigns the minimum threshold of occurrence to include species in the c-score
+# Assign the minimum threshold of occurrence to include species in the c-score
 # calculations
 occurrence.tresh <- 1
 
-# Builds the c-score dataset  
+# Assign the criterion for standardization of c-scores
+standardize <- TRUE
+
+# Assign the criterion for generating a null-model for the c-scores
+null.model <- TRUE
+
+# Assign the number of randomizations for creating the null-model 
+rand = 9999
+
+# Build the c-score dataset  
 dataset <- cScorePairWisePerGroup(m = m, time = time, site = site, 
                     spp.group = spp.group, occurrence.tresh = occurrence.tresh, 
                     standardize = TRUE, null.model = TRUE, rand = 9999)
